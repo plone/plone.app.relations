d70b5072615082510ecd40f7d2c75d9f9e158432
Alec Mitchell <apm13@columbia.edu>
2007-10-04 02:45:22 +0000

    Update egg deps
    svn path=/plone.app.relations/trunk/; revision=17417

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='plone.app.relations',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          "plone.relations",
       ],
       entry_points="""
       # -*- Entry points: -*-
