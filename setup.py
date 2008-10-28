93db5fdaad2ac00f8ec76eb4916af2e43ebe428a
Helge Tesdal <tesdal@jarn.com>
2008-10-28 15:36:21 +0000

    plone is also in namespace packages
    svn path=/plone.app.relations/trunk/; revision=23034

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='plone.app.relations',
       url='http://svn.plone.org/svn/plone/plone.app.relations',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone.app'],
+      namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
