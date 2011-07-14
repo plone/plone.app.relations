f87fa3383ec150ac0eb840b6071e5bf69b3da9fe
Hanno Schlichting <hanno@hannosch.eu>
2011-07-14 12:52:10 +0000

    Update metadata
    svn path=/plone.app.relations/trunk/; revision=51253

--- README.txt
+++ README.txt
@@ -1,10 +1,9 @@
-plone.app.relations Package Readme
-==================================
-
 Overview
---------
+========
 
-A set of components to provide a content centric API for the engine from plone.relations, as well as a few different core relationship types and policies.  See the package README.txt for details on usage.
+A set of components to provide a content centric API for the engine from
+plone.relations, as well as a few different core relationship types and
+policies. See the package README.txt for details on usage.
 
 Credits
 -------
@@ -15,4 +14,3 @@ Based on the index and relationship container from zc.relationship by
 Gary Poster from Zope Corporation.
 
 This work was partly sponsored by The Daily Reel (http://www.thedailyreel.com)
-
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '2.0'
 
 setup(name='plone.app.relations',
       version=version,
-      description="A set of components to provide a content centric API for the engine from plone.relations, as well as a few different core relationship types and policies.",
-      long_description="""\
-""",
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description="A set of components to provide a content centric API for "
+        "the engine from plone.relations, as well as a few different core "
+        "relationship types and policies.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Zope2",
-        "Framework :: Zope3",
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='relationships references plone',
-      author='Alec Mitchell',
-      author_email='apm13@columbia.edu',
-      url='http://svn.plone.org/svn/plone/plone.app.relations',
-      license='GPL',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.relations',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
@@ -28,6 +28,7 @@ setup(name='plone.app.relations',
           "plone.relations",
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
