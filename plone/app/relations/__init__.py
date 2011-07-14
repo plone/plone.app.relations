6500dd8ce685da461e3566ab4309fd258a258162
Hanno Schlichting <hanno@hannosch.eu>
2011-07-14 12:56:59 +0000

    Register GS profile via ZCML
    svn path=/plone.app.relations/trunk/; revision=51255

--- plone/app/relations/__init__.py
+++ plone/app/relations/__init__.py
@@ -1,6 +1,3 @@
-from Products.GenericSetup import EXTENSION, profile_registry
-from Products.CMFCore.interfaces import ISiteRoot
-
 from Products.PluggableAuthService import registerMultiPlugin
 from Products.CMFCore.permissions import ManagePortal
 import local_role
@@ -14,11 +11,3 @@ def initialize(context):
                           constructors = (local_role.manage_addRelationshipLocalRoleManagerForm,
                                           local_role.manage_addRelationshipLocalRoleManager),
                           visibility = None)
-
-    profile_registry.registerProfile('default',
-                                     'plone.app.relations',
-                                     'Extension profile for plone.app.relatons',
-                                     'profiles/default',
-                                     'plone.app.relations',
-                                     EXTENSION,
-                                     for_=ISiteRoot)
